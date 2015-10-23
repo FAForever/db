33669c142b358c2b5d68d8b687ca7a5d8436fbf9
Sheeo <sheeo@faforever.com>
2015-10-23 12:36:29 +0200

    Add sync database initialization code

--- faf/db/__init__.py
+++ faf/db/__init__.py
@@ -1,3 +1,13 @@
 """
-Contains server side faf database abstraction
+Synchronous faf database abstraction
+
+Just PyMYSQL.
 """
+import pymysql
+
+connection = None
+
+
+def init_db(config):
+    global connection
+    connection = pymysql.connect(**config['DATABASE'])
