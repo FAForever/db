137e76812f74ed90913867d69ddc0dc825c597cb
Sheeo <sheeo@faforever.com>
2015-10-27 21:15:28 +0100

    Override connection.Cursor() to reconnect

--- faf/db/__init__.py
+++ faf/db/__init__.py
@@ -11,3 +11,11 @@ connection = None
 def init_db(config):
     global connection
     connection = pymysql.connect(**config['DATABASE'])
+
+    def get_cursor():
+        """
+        Ping the connection every time we get a cursor, will if the connection is dead.
+        """
+        connection.ping()
+        return pymysql.connections.Connection.cursor(connection)
+    connection.cursor = get_cursor
