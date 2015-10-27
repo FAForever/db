8aac770de57964fdcdad821ac7b40e14cbc633c1
Sheeo <sheeo@faforever.com>
2015-10-27 21:39:53 +0100

    Allow passing a cursorclass

--- faf/db/__init__.py
+++ faf/db/__init__.py
@@ -12,10 +12,10 @@ def init_db(config):
     global connection
     connection = pymysql.connect(**config['DATABASE'])
 
-    def get_cursor():
+    def get_cursor(cursor=None):
         """
         Ping the connection every time we get a cursor, will if the connection is dead.
         """
         connection.ping()
-        return pymysql.connections.Connection.cursor(connection)
+        return pymysql.connections.Connection.cursor(connection, cursor)
     connection.cursor = get_cursor
