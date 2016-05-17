ad15eff57d8c88cd39984c5aa26ef71d68e7382f
Dragonfire <bastian616-git@yahoo.de>
2016-05-17 03:39:04 +0200

    Moved to API

--- migrations/add_clans-dab3d56fe949550d2ac113a148582e4c7c690e25c.sql
+++ migrations/add_clans-dab3d56fe949550d2ac113a148582e4c7c690e25c.sql
@@ -18,30 +18,3 @@ CREATE TABLE `clan_members` (
   PRIMARY KEY (`clan_id`,`player_id`),
   KEY `player_id` (`player_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=48 DEFAULT CHARSET=utf8;
-
-CREATE 
-    ALGORITHM = UNDEFINED 
-    DEFINER = `root`@`localhost` 
-    SQL SECURITY DEFINER
-VIEW `clans` AS
-    SELECT 
-        `clan_id` AS `clan_id`,
-        `status` AS `status`,
-        `clan_name` AS `clan_name`,
-        `clan_tag` AS `clan_tag`,
-        `clan_leader_id` AS `clan_leader_id`,
-        `clan_founder_id` AS `clan_founder_id`,
-        `clan_desc` AS `clan_desc`,
-        `create_date` AS `create_date`,
-        `leader`.`login` AS `leader_name`,
-        `founder`.`login` AS `founder_name`,
-        (SELECT 
-                COUNT(0)
-            FROM
-                `clan_members`
-            WHERE
-                (`clan_members`.`clan_id` = `clan_list`.`clan_id`)) AS `member_count`
-    FROM
-        `clan_list`
-        LEFT JOIN `login` `leader` ON (`clan_leader_id` = `leader`.`id`)
-        LEFT JOIN `login` `founder` ON (`clan_founder_id` = `founder`.`id`);
