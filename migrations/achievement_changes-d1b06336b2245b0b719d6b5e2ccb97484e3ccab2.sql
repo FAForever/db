53e7c771cdeccd66a7288192efb0bcb78143e4c9
Michel Jung <michel.jung89@gmail.com>
2016-06-24 09:41:55 +0200

    Fix migration scripts

--- migrations/achievement_changes-d1b06336b2245b0b719d6b5e2ccb97484e3ccab2.sql
+++ migrations/achievement_changes-d1b06336b2245b0b719d6b5e2ccb97484e3ccab2.sql
@@ -3,7 +3,7 @@ UPDATE achievement_definitions set total_steps = 500 where id = 'e7645e7c-7456-4
 UPDATE achievement_definitions set total_steps = 500 where id = 'd38aec23-e487-4aa2-899e-418e29ffbd36';
 UPDATE achievement_definitions set total_steps = 500 where id = 'f0cde5d8-4933-4074-a2fb-819074d21abd';
 UPDATE achievement_definitions set total_steps = 500 where id = 'a6b7dfa1-1ebc-4c6d-9305-4a9d623e1b4f';
-UPDATE achievement_definitions set nameKey = 'achievement.riptide.title', description_key = 'achievement.riptide.description' where id = '326493d7-ce2c-4a43-bbc8-3e990e2685a1';
+UPDATE achievement_definitions set name_key = 'achievement.riptide.title', description_key = 'achievement.riptide.description' where id = '326493d7-ce2c-4a43-bbc8-3e990e2685a1';
 
 UPDATE messages set `value` = 'Build 500 Air Superiority Fighter in a single game' where `key` = 'achievement.whatASwarm.description';
 UPDATE messages set `value` = 'Build 500 fire beetles in total' where `key` = 'achievement.deadlyBugs.description';
