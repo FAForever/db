1f79d64fe87fde3f9b9591e498e7c7cb20cbf1cc
HardlySoftly <HardlySoftly@gmail.com>
2016-09-09 20:58:55 +0100

    Swap to RGB values for clan tag colours in the DB
    And stick with varchar, because we dislike unvarchars

--- migrations/add_clan_tag_colors-150bb43b63f44549c415ae56853632f8779f8cd4.sql
+++ migrations/add_clan_tag_colors-150bb43b63f44549c415ae56853632f8779f8cd4.sql
@@ -1,1 +1,1 @@
-ALTER TABLE clan_list ADD COLUMN `clan_tag_color` varchar(30) DEFAULT NULL COMMENT 'Color of the clan''s tag in chat';
+ALTER TABLE clan_list ADD COLUMN `clan_tag_color` varchar(6) DEFAULT NULL COMMENT 'RGB color code for the clan''s tag in chat';
