f89e2a6ca71e5b559875d72ca94e68d3cd26a5d0
Lukas Erlacher <erlacher@in.tum.de>
2016-11-19 14:44:39 +0100

    Add schema dump to build
    * Add `-d` for "dump schema" option to `setup_db.sh`
    * Use `-d` in travis build

--- setup_db.sh
+++ setup_db.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
 
+DUMPSCHEMA=
+
+while getopts ":d" opt; do
+  case $opt in
+    d)
+      DUMPSCHEMA=1
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      ;;
+  esac
+done
+
 # Builds and runs the faf-db image in a new container and waits until it initialized
 
 docker build -t faf-db .
@@ -17,5 +30,9 @@ docker exec -i ${DB_CONTAINER} mysql -uroot -pbanana -e "create database faf_tes
 echo "y
 n
 " | ./migrate.sh faf-db || exit 1;
+
+if [ $DUMPSCHEMA ]; then
+  docker exec -i ${DB_CONTAINER} mysqldump -uroot -pbanana --no-data faf_test || exit 1;
+fi
 
 docker exec -i ${DB_CONTAINER} mysql -uroot -pbanana faf_test < db-data.sql || exit 1;
