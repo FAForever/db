535c0b25f854f22f78970eba92286fe20137e53e
Cheyan Setayesh <cheyan.set@gmail.com>
2017-01-09 22:00:26 -0500

    remove docker toolbox check, add more comments, add support for schema dump to host

--- setup_db.sh
+++ setup_db.sh
@@ -1,54 +1,52 @@
 #!/usr/bin/env bash
 
-DUMPSCHEMA=
+DUMP_SCHEMA_TO_CONTAINER_STDOUT=
+COPY_SCHEMA_TO_HOST_FILE=
 
-while getopts ":d" opt; do
+while getopts ":dc" opt; do
   case $opt in
     d)
-      DUMPSCHEMA=1
+      DUMP_SCHEMA_TO_CONTAINER_STDOUT=1
+      ;;
+    c)
+      COPY_SCHEMA_TO_HOST_FILE=1
       ;;
     \?)
       echo "Invalid option: -$OPTARG" >&2
+      exit 1;
       ;;
   esac
 done
 
-# Builds and runs the faf-db image in a new container and waits until it initialized
+# Check if docker is running
+docker version 1>/dev/null || exit 1;
 
+# Builds image
 docker build -t faf-db .
-DB_CONTAINER=$(docker run -d --name faf-db -e MYSQL_ROOT_PASSWORD=banana -p 3306:3306 faf-db)
+DB_CONTAINER=$(docker run -d --name faf-db -e MYSQL_ROOT_PASSWORD=banana -p 3306:3306 faf-db) || exit 1;
 
-if [[ $DOCKER_MACHINE_NAME ]]; then
-  echo "Docker Toolbox is not supported by this script"
-  exit 1;
-fi
-
-DB_ADDRESS=
-if [[ $OSTYPE == darwin* ]]; then
-  DB_ADDRESS="127.0.0.1"
-else
-  DB_ADDRESS=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ${DB_CONTAINER})
-fi
-
-echo "Waiting for mysql container..."
-until nc -z ${DB_ADDRESS} 3306
-do
-  sleep 1
-done
-
+# Run faf-db image in a new container and waits until it initialized
 echo "Creating database faf_test..."
-until docker exec -i ${DB_CONTAINER} mysql -uroot -pbanana -e "create database faf_test;"
+until docker exec -i ${DB_CONTAINER} mysql -uroot -pbanana -e "create database faf_test;" 2>/dev/null;
 do
-  echo "Mysql still initializing, retrying in 10 seconds"
-  sleep 10
+  echo "Mysql still initializing, retrying in 10 seconds";
+  sleep 10;
 done
+echo "Successfully created database faf_test";
 
 echo "y
 n
 " | ./migrate.sh faf-db || exit 1;
 
-if [ $DUMPSCHEMA ]; then
+if [ $DUMP_SCHEMA_TO_CONTAINER_STDOUT ]; then
   docker exec -i ${DB_CONTAINER} mysqldump -uroot -pbanana --no-data faf_test || exit 1;
+fi
+
+if [ $COPY_SCHEMA_TO_HOST_FILE ]; then
+  docker exec -i ${DB_CONTAINER} /bin/sh -c "mysqldump -uroot -pbanana --no-data faf_test > /tmp/dump.sql" || exit 1;
+  docker cp ${DB_CONTAINER}:/tmp/dump.sql /tmp/dump.sql || exit 1;
+  docker exec -i ${DB_CONTAINER} rm -f /tmp/dump.sql || exit 1;
+  echo "Schema copied to /tmp/dump.sql"
 fi
 
 docker exec -i ${DB_CONTAINER} mysql -uroot -pbanana faf_test < db-data.sql || exit 1;
