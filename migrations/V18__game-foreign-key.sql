ebd4160a042f3cd2d713f80b499118c80eea4303
Michel Jung <michel.jung89@gmail.com>
2017-03-13 22:05:46 +0100

    Try if migration succeeds with foreign key checks disabled Migration failed with: SQL State  : HY000 Error Code : 1025 Message    : (conn:10) Error on rename of './faf_lobby/#sql-1_a' to './faf_lobby/game_player_stats' (errno: 168 - Unknown (generic) error from engine)

--- migrations/V18__game-foreign-key.sql
+++ migrations/V18__game-foreign-key.sql
@@ -1,5 +1,10 @@
 DELETE FROM game_player_stats where gameId not in (select id from game_stats);
+
+SET FOREIGN_KEY_CHECKS=0;
+
 ALTER TABLE game_player_stats
   MODIFY gameId INT(10) unsigned NOT NULL,
-  ADD CONSTRAINT game_id_fk FOREIGN KEY (gameId) REFERENCES game_stats (id)
+  ADD CONSTRAINT game_id_fk FOREIGN KEY (gameId) REFERENCES game_stats (id);
+
+SET FOREIGN_KEY_CHECKS=1;
 
