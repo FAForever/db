6a6e1a9ea2792412e73ca131c189b726ea30d226
Michel Jung <michel.jung89@gmail.com>
2017-10-03 20:14:24 +0200

    Separate users for Java and Python server/api, add faf-mysql-exporter

--- init/0_init_db_and_users.sh
+++ init/0_init_db_and_users.sh
@@ -12,13 +12,15 @@ create() {
 SQL_SCRIPT
 }
 
-create "faf" "faf-api" "${MYSQL_API_PASSWORD}"
+create "faf" "faf-java-api" "${MYSQL_JAVA_API_PASSWORD}"
+create "faf" "faf-python-api" "${MYSQL_PYTHON_API_PASSWORD}"
 create "faf" "faf-legacy-apps" "${MYSQL_LEGACY_APPS_PASSWORD}"
 create "faf" "faf-legacy-live-replay-server" "${MYSQL_LEGACY_LIVE_REPLAY_SERVER_PASSWORD}"
 create "faf" "faf-legacy-secondary-server" "${MYSQL_LEGACY_SECONDARY_SERVER_PASSWORD}"
 create "faf" "faf-legacy-updater" "${MYSQL_LEGACY_UPDATER_PASSWORD}"
 create "faf-murmur" "faf-murmur" "${MYSQL_MURMUR_PASSWORD}"
-create "faf" "faf-server" "${MYSQL_SERVER_PASSWORD}"
+create "faf" "faf-java-server" "${MYSQL_JAVA_SERVER_PASSWORD}"
+create "faf" "faf-python-server" "${MYSQL_PYTHON_SERVER_PASSWORD}"
 create "faf-softvote" "faf-softvote" "${MYSQL_SOFTVOTE_PASSWORD}"
 create "faf-anope" "faf-anope" "${MYSQL_ANOPE_PASSWORD}"
 create "faf-wiki" "faf-wiki" "${MYSQL_WIKI_PASSWORD}"
@@ -29,4 +31,11 @@ create "faf-wordpress" "faf-wordpress" "${MYSQL_WORDPRESS_PASSWORD}"
 # consider worse than giving faf-server more permissions than it needs.
 mysql --user=root --password=${MYSQL_ROOT_PASSWORD} <<SQL_SCRIPT
     GRANT ALL PRIVILEGES ON \`faf-anope\`.* TO 'faf-server'@'%';
+SQL_SCRIPT
+
+# Allows faf-mysql-exporter to read metrics. It is recommended to set a max connection limit for the user to avoid
+# overloading the server with monitoring scrapes under heavy load.
+mysql --user=root --password=${MYSQL_ROOT_PASSWORD} <<SQL_SCRIPT
+  CREATE USER 'faf-mysql-exporter'@'%' IDENTIFIED BY '${MYSQL_MYSQL_EXPORTER_PASSWORD}' WITH MAX_USER_CONNECTIONS 3;
+  GRANT PROCESS, REPLICATION CLIENT, SELECT ON *.* TO 'faf-mysql-exporter'@'%';
 SQL_SCRIPT
