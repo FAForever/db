9249027e8672602063130e9b8b2b9d99986ecf66
Michel Jung <michel.jung89@gmail.com>
2017-10-27 23:42:51 +0200

    Fix achievement texts

---
+++ migrations/V45__update-achievement-texts.sql
@@ -0,0 +1,15 @@
+UPDATE messages
+SET value = 'Win a ranked 1v1 game in under 10 minutes'
+WHERE `key` = 'achievement.rusher.description' AND region = 'US' AND language = 'en';
+
+UPDATE messages
+SET value = 'Be the top scoring player in a game with at least 8 players'
+WHERE `key` = 'achievement.topScore.description' AND region = 'US' AND language = 'en';
+
+UPDATE messages
+SET value = 'Be the top scoring player in a game with at least 8 players'
+WHERE `key` = 'achievement.topScore.description' AND region = 'US' AND language = 'en';
+
+UPDATE messages
+SET value = 'Be the top scoring player 10 times in games with at least 8 players'
+WHERE `key` = 'achievement.unbeatable.description' AND region = 'US' AND language = 'en';
