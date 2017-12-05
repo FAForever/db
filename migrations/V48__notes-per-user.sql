7eaf3c4c7a26bbb248060fc92c8869dd872b08d2
Christian Schmidt <c-schmidt@gmx.eu>
2017-12-06 00:36:20 +0100

    fixes #146 moderator notes per user

---
+++ migrations/V48__notes-per-user.sql
@@ -0,0 +1,16 @@
+CREATE TABLE user_notes
+(
+    id INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    user_id MEDIUMINT(9) unsigned NOT NULL,
+    author MEDIUMINT(9) unsigned NOT NULL,
+    watched TINYINT(4) DEFAULT '0' NOT NULL,
+    note TEXT NOT NULL,
+    create_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    update_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    CONSTRAINT user_notes_user_login_id_fk FOREIGN KEY (user_id) REFERENCES login (id),
+    CONSTRAINT user_notes_author_login_id_fk FOREIGN KEY (author) REFERENCES login (id)
+);
+CREATE INDEX user_notes_user_login_id_fk ON user_notes (user_id);
+CREATE INDEX user_notes_author_login_id_fk ON user_notes (author);
+CREATE INDEX user_notes_watched_index ON user_notes (watched);
+ALTER TABLE user_notes COMMENT = 'Notes taken by moderators';
