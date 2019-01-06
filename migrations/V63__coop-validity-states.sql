5de6e8cc294c3864bd9f2cbec19ccaa3d80dd816
Askaholic <askaholic907@gmail.com>
2019-01-06 12:57:14 -0900

    Add validity states for coop leaderboards

---
+++ migrations/V63__coop-validity-states.sql
@@ -0,0 +1,5 @@
+INSERT INTO game_validity (id, message) VALUES
+  (20, 'Civilians were revealed'),
+  (21, 'Difficulty was too low'),
+  (22, 'Expansion was disabled'),
+  (23, 'Team spawn was not fixed');
