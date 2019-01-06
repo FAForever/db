49dad3de191fb10ce4187ba25409afa346c95602
Askaholic <askaholic907@gmail.com>
2019-01-06 13:09:55 -0900

    Change difficulty text

--- migrations/V63__coop-validity-states.sql
+++ migrations/V63__coop-validity-states.sql
@@ -1,5 +1,5 @@
 INSERT INTO game_validity (id, message) VALUES
   (20, 'Civilians were revealed'),
-  (21, 'Difficulty was too low'),
+  (21, 'Difficulty was wrong'),
   (22, 'Expansion was disabled'),
   (23, 'Team spawn was not fixed');
