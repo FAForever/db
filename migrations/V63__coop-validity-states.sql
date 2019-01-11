2c12f9305f3766a0d104373cd8e965f3b4427f81
Askaholic <askaholic907@gmail.com>
2019-01-11 18:51:46 +0000

    Add validity states for coop leaderboards (#187)
    * Add validity states for coop leaderboards
    * Add OTHER_UNRANK

---
+++ migrations/V63__coop-validity-states.sql
@@ -0,0 +1,6 @@
+INSERT INTO game_validity (id, message) VALUES
+  (20, 'Civilians were revealed'),
+  (21, 'Difficulty was wrong'),
+  (22, 'Expansion was disabled'),
+  (23, 'Team spawn was not fixed'),
+  (24, 'Unranked for another reason');
