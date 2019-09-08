24ab29e92447094932a0550c33f7f4248a7152bf
Askaholic <askaholic907@gmail.com>
2019-09-07 16:49:05 -0800

    Change gameuid type first

--- migrations/V75__coop_leaderboard_foreign_key.sql
+++ migrations/V75__coop_leaderboard_foreign_key.sql
@@ -3,5 +3,6 @@ DELETE c FROM coop_leaderboard c
   LEFT JOIN game_stats g ON g.id = c.gameuid
       WHERE g.id IS NULL;
 
+ALTER TABLE coop_leaderboard MODIFY gameuid INT UNSIGNED;
 ALTER TABLE coop_leaderboard
   ADD FOREIGN KEY (gameuid) REFERENCES game_stats(id);
