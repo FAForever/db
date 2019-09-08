a3de0c642ca4462a4e0cd99e5d11de06992c2a64
Konstantin Kalinovsky <konstantin.kalinovsky@firma.seznam.cz>
2019-09-08 21:25:42 +0200

    That migrations are needed for https://github.com/FAForever/faf-java-api/pull/328

---
+++ migrations/V76__updates_fafdevelop.sql
@@ -0,0 +1,18 @@
+
+CREATE TABLE IF NOT EXISTS `updates_fafdevelop_files` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `fileId` smallint(5) unsigned NOT NULL,
+  `version` int(11) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `md5` varchar(45) NOT NULL,
+  `obselete` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `fileId` (`fileId`)
+) ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS `updates_fafdevelop` (
+  `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
+  `filename` varchar(45) NOT NULL,
+  `path` varchar(45) NOT NULL,
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB;
