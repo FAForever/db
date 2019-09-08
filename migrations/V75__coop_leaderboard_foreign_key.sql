82ca3b1171d7218a314e36a791f75b5ad4d3ef72
Askaholic <askaholic907@gmail.com>
2019-09-07 16:32:42 -0800

    Add foreign key to coop leaderboards gameuid

---
+++ migrations/V75__coop_leaderboard_foreign_key.sql
@@ -0,0 +1,7 @@
+-- Delete bad values
+DELETE c FROM coop_leaderboard c
+  LEFT JOIN game_stats g ON g.id = c.gameuid
+      WHERE g.id IS NULL;
+
+ALTER TABLE coop_leaderboard
+  ADD FOREIGN KEY (gameuid) REFERENCES game_stats(id);
