b483bb0b89fe96a3d4eee93813418ed34dafe518
Brutus5000 <brutus5000@gmx.net>
2020-01-28 22:57:59 +0100

    Add schemaspy

---
+++ ci/schemaspy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+echo 'travis_fold:start:schemaspy'
+
+echo '# Generating db diagram with Schemaspy'
+
+# Must be generated into the folder 'public' as we're going to use it as GitLab pages
+# See https://gitlab.com/help/user/project/pages/index.md#how-it-works
+SCHEMASPY_OUTPUT_DIR=$PWD/public
+
+cat "$PWD/ci/schemaspy.properties"
+
+mkdir -p "${SCHEMASPY_OUTPUT_DIR}"
+chmod a+rw "${SCHEMASPY_OUTPUT_DIR}"
+
+docker run --network="faf" \
+            -v "$PWD/ci/schemaspy.properties:/schemaspy.properties" \
+            -v "${SCHEMASPY_OUTPUT_DIR}:/output" \
+            schemaspy/schemaspy:latest \
+           || { echo "Could not generate schema"; exit 1; }
+
+echo 'travis_fold:end:schemaspy'
