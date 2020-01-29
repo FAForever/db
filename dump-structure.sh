eb856875ab4cd542bc7441c3382c6116a5d92e0b
Brutus5000 <brutus5000@gmx.net>
2020-01-29 19:26:38 +0100

    Delete obsolete scripts

--- dump-structure.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-if [ ! "${MYSQL_ROOT_PASSWORD+1}" ]; then
-  echo "This script is not intended for use outside the docker container (variable MYSQL_ROOT_PASSWORD has not been set)"
-  echo "Try instead: docker -i faf-db ${0}"
-  exit 1
-fi
-
-mysqldump -uroot -p${MYSQL_ROOT_PASSWORD} --no-data --no-defaults faf || exit 1;
--- faf/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-Required to make faf work as a namespace package on Python < 3.3
-"""
-from pkgutil import extend_path
-__path__ = extend_path(__path__, __name__)
