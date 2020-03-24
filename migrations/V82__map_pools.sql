ff152928fb44a7fd8f47e429effcf946264afec6
Askaholic <askaholic907@gmail.com>
2020-03-23 17:11:20 -0800

    Allow map pools to be applied to multiple matchmaker pools

--- migrations/V82__map_pools.sql
+++ migrations/V82__map_pools.sql
@@ -2,9 +2,6 @@ CREATE TABLE map_pool
 (
     id                  INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name                VARCHAR(255) NOT NULL,
-    matchmaker_pool_id  INT REFERENCES matchmaker_pool (id),
-    min_rating          INT,
-    max_rating          INT,
     create_time         TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     update_time         TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB
@@ -16,5 +13,14 @@ CREATE TABLE map_pool_maps
     map_id        INT NOT NULL REFERENCES map_version (id),
     PRIMARY KEY (map_pool_id, map_id),
     INDEX (map_id, map_pool_id)
-)
-COMMENT="Map pool many-to-many table";
+);
+
+CREATE TABLE matchmaker_pool_map_pools
+(
+    matchmaker_pool_id  INT REFERENCES matchmaker_pool (id),
+    map_pool_id         INT NOT NULL REFERENCES map_pool (id),
+    min_rating          INT,
+    max_rating          INT,
+    PRIMARY KEY (matchmaker_pool_id, map_pool_id),
+    INDEX (map_pool_id, matchmaker_pool_id)
+);
