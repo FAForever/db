30ca90af7378402eef47cee4715721cc64336804
Konstantin Kalinovsky <konstantin.kalinovsky@firma.seznam.cz>
2020-04-20 02:26:18 +0200

    Migrate map_reviews_summary, ladder1v1_rating_distribution and global_rating_distribution views to tables on events

---
+++ migrations/V89__global_rating_distribution_view_to_event.sql
@@ -0,0 +1,43 @@
+DROP VIEW `global_rating_distribution`;
+
+CREATE TABLE IF NOT EXISTS `global_rating_distribution` (
+    rating DOUBLE PRIMARY KEY,
+    count BIGINT
+) ENGINE=InnoDB;
+
+ALTER TABLE `global_rating`
+ADD INDEX `global_rating_idx_for_rating_distribution` (`is_active`, `numGames`, `mean`, `deviation`);
+
+INSERT INTO global_rating_distribution
+SELECT `subq`.`rating`, COUNT(*)
+FROM (
+  SELECT floor((`global_rating`.`mean` - 3 * `global_rating`.`deviation`) / 100) * 100 AS `rating`
+  FROM `global_rating`
+  WHERE `global_rating`.`is_active` = 1 AND `global_rating`.`numGames` > 0
+) AS `subq`
+GROUP BY `subq`.`rating`
+ORDER BY `subq`.`rating`;
+
+DELIMITER $$
+
+CREATE EVENT IF NOT EXISTS `global_rating_distribution`
+ON SCHEDULE EVERY 1 HOUR
+COMMENT 'Recompute the global_rating_distribution table every hour'
+DO
+    BEGIN
+      START TRANSACTION;
+      DELETE FROM global_rating_distribution;
+
+      INSERT INTO global_rating_distribution
+      SELECT `subq`.`rating`, COUNT(*)
+      FROM (
+        SELECT floor((`global_rating`.`mean` - 3 * `global_rating`.`deviation`) / 100) * 100 AS `rating`
+        FROM `global_rating`
+        WHERE `global_rating`.`is_active` = 1 AND `global_rating`.`numGames` > 0
+      ) AS `subq`
+      GROUP BY `subq`.`rating`
+      ORDER BY `subq`.`rating`;
+      COMMIT;
+    END $$
+
+DELIMITER ;
