18ac1c8e5fcae1b5e1c67f9c4aaa50d1118a0c85
Brutus5000 <Brutus5000@gmx.net>
2020-05-25 20:48:36 +0200

    Add a login log to discover login attack attempts

---
+++ migrations/V94__login_log.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS `login_log` (
+    `id` INT UNSIGNED NOT NULL UNIQUE AUTO_INCREMENT COMMENT 'ID for API/Elide compatibility',
+    `login_id` MEDIUMINT UNSIGNED NOT NULL,
+    `ip` VARCHAR(45) NOT NULL,
+    `attempts` MEDIUMINT UNSIGNED NOT NULL DEFAULT 1,
+    `success` BOOLEAN NOT NULL,
+    `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`success`,`ip`,`login_id`),
+    CONSTRAINT FOREIGN KEY (`login_id`) REFERENCES `login` (`id`)
+) ENGINE=InnoDB COMMENT='Log of logins to discover login attack attempts. Has to be cleaned up by the services.';
