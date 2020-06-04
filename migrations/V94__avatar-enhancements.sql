6451ffa73f537a409620c7e51ef3c0ee6c1ab86e
Ahsan Bagwan <ahsanbagwan@gmail.com>
2020-06-04 17:33:05 +0530

    #368 - Add generated column among others

--- migrations/V94__avatar-enhancements.sql
+++ migrations/V94__avatar-enhancements.sql
@@ -1,2 +1,9 @@
-ALTER TABLE avatars_list
-    CHANGE `url` `filename` VARCHAR(255);
+ALTER TABLE avatars_list ADD COLUMN `filename` VARCHAR(255);
+
+UPDATE avatars_list SET filename=REPLACE(url, 'https://content.faforever.com/faf/avatars/', '');
+
+ALTER TABLE avatars_list MODIFY `filename` VARCHAR(255) NOT NULL;
+
+ALTER TABLE avatars_list DROP COLUMN `url`;
+
+ALTER TABLE `avatars_list` ADD COLUMN `url` VARCHAR(255) GENERATED ALWAYS AS (CONCAT('https://content.faforever.com/faf/avatars/', filename)) STORED;
