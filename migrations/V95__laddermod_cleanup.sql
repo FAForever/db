50c0d68ae6381bc9956f2fca4db9fa077858dd0c
Clemens Borys <clemens@borys.dk>
2020-06-10 00:20:52 +0200

    Save player count in temporary table

--- migrations/V95__laddermod_cleanup.sql
+++ migrations/V95__laddermod_cleanup.sql
@@ -23,20 +23,32 @@ AND
   ) = 2
 ;
 
+
+DROP TEMPORARY TABLE IF EXISTS faf.game_player_count;
+
+CREATE TEMPORARY TABLE faf.game_player_count
+    SELECT
+      faf.game_stats.id AS id,
+      count(faf.game_player_stats.id) AS gps_row_count,
+      count(DISTINCT faf.game_player_stats.playerId) AS distinct_player_count
+    FROM 
+      faf.game_stats
+    INNER JOIN 
+      faf.game_player_stats ON (faf.game_player_stats.gameId = faf.game_stats.id)
+    WHERE
+      faf.game_stats.id BETWEEN 4393127 AND 4424727
+    GROUP BY
+      faf.game_stats.id
+;
+
 UPDATE
   faf.game_stats
+INNER JOIN
+  faf.game_player_count ON (faf.game_stats.id = faf.game_player_count.id)
 SET
   validity = 24  # "Unranked for another reason"
 WHERE
   validity = 0
 AND
-  (
-    SELECT count(*) FROM faf.game_player_stats
-    WHERE gameId = faf.game_stats.id
-  ) != (
-    SELECT count(DISTINCT faf.game_player_stats.playerId) FROM faf.game_player_stats
-    WHERE gameId = faf.game_stats.id
-  )
-AND
-  faf.game_stats.id BETWEEN 4393127 AND 4424727
+  gps_row_count != distinct_player_count
 ;
