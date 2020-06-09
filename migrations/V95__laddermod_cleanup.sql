87b9265f9307e7e01e4643d81b3fe3e53c16bc87
Clemens Borys <clemens@borys.dk>
2020-06-09 18:19:13 +0200

    Add gameMod=6 to old ladder games and make games with repeated players invalid

---
+++ migrations/V95__laddermod_cleanup.sql
@@ -0,0 +1,44 @@
+UPDATE
+  faf.game_stats
+SET
+  gamemod = 6
+WHERE
+  gamemod = 0
+AND
+  startTime < "2014-03-22 12:59:25"
+AND
+  gameName LIKE _utf8 "% Vs %" COLLATE utf8_bin
+AND
+  (
+    SELECT count(*) FROM faf.game_player_stats
+    WHERE gameId = faf.game_stats.id
+  ) = 2
+AND
+  (
+    SELECT count(*) FROM faf.game_player_stats
+    WHERE gameId = faf.game_stats.id AND
+      AI = false AND
+      color IN (1, 2) AND
+      score in (-1, 0, 1)
+  ) = 2
+;
+
+UPDATE
+  faf.game_stats
+SET
+  validity = 24  # "Unranked for another reason"
+WHERE
+  validity = 0
+AND
+  (
+    SELECT count(*) FROM faf.game_player_stats
+    WHERE gameId = faf.game_stats.id
+  ) != (
+    SELECT count(DISTINCT faf.game_player_stats.playerId) FROM faf.game_player_stats
+    WHERE gameId = faf.game_stats.id
+  )
+AND
+  faf.game_stats.id > 4393127
+AND
+  faf.game_stats.id < 4424727
+;
