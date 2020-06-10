66e2b93e88cc341f20c0bdb5c1fb59d42732dca0
Clemens Borys <clemens@borys.dk>
2020-06-10 12:47:38 +0200

    Remove database prefixes

--- migrations/V95__laddermod_cleanup.sql
+++ migrations/V95__laddermod_cleanup.sql
@@ -1,5 +1,5 @@
 UPDATE
-  faf.game_stats
+  game_stats
 SET
   gamemod = 6
 WHERE
@@ -10,13 +10,13 @@ AND
   gameName LIKE "% Vs %"
 AND
   (
-    SELECT count(*) FROM faf.game_player_stats
-    WHERE gameId = faf.game_stats.id
+    SELECT count(*) FROM game_player_stats
+    WHERE gameId = game_stats.id
   ) = 2
 AND
   (
-    SELECT count(*) FROM faf.game_player_stats
-    WHERE gameId = faf.game_stats.id AND
+    SELECT count(*) FROM game_player_stats
+    WHERE gameId = game_stats.id AND
       AI = false AND
       color IN (1, 2) AND
       score in (-1, 0, 1)
@@ -24,27 +24,27 @@ AND
 ;
 
 
-DROP TEMPORARY TABLE IF EXISTS faf.game_player_count;
+DROP TEMPORARY TABLE IF EXISTS game_player_count;
 
-CREATE TEMPORARY TABLE faf.game_player_count
+CREATE TEMPORARY TABLE game_player_count
     SELECT
-      faf.game_stats.id AS id,
-      count(faf.game_player_stats.id) AS gps_row_count,
-      count(DISTINCT faf.game_player_stats.playerId) AS distinct_player_count
+      game_stats.id AS id,
+      count(game_player_stats.id) AS gps_row_count,
+      count(DISTINCT game_player_stats.playerId) AS distinct_player_count
     FROM 
-      faf.game_stats
+      game_stats
     INNER JOIN 
-      faf.game_player_stats ON (faf.game_player_stats.gameId = faf.game_stats.id)
+      game_player_stats ON (game_player_stats.gameId = game_stats.id)
     WHERE
-      faf.game_stats.id BETWEEN 4393127 AND 4424727
+      game_stats.id BETWEEN 4393127 AND 4424727
     GROUP BY
-      faf.game_stats.id
+      game_stats.id
 ;
 
 UPDATE
-  faf.game_stats
+  game_stats
 INNER JOIN
-  faf.game_player_count ON (faf.game_stats.id = faf.game_player_count.id)
+  game_player_count ON (game_stats.id = game_player_count.id)
 SET
   validity = 24  # "Unranked for another reason"
 WHERE
