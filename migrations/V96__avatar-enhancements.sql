c9e46d9437d22c9efbdbb9cdaf44d990f9089934
Ahsan Bagwan <ahsanbagwan@gmail.com>
2020-06-10 21:36:21 +0530

    #368 - Incorporate changes based on PR comments

--- migrations/V96__avatar-enhancements.sql
+++ migrations/V96__avatar-enhancements.sql
@@ -1,13 +1,13 @@
-ALTER TABLE avatars_list ADD COLUMN `filename` VARCHAR(255);
+ALTER TABLE avatars_list ADD COLUMN `filename` VARCHAR(212);
 
 UPDATE avatars_list SET filename=REPLACE(url, 'https://content.faforever.com/faf/avatars/', '');
 
-ALTER TABLE avatars_list MODIFY `filename` VARCHAR(255) NOT NULL;
+ALTER TABLE avatars_list MODIFY `filename` VARCHAR(212) NOT NULL;
 
 ALTER TABLE avatars_list DROP INDEX `url`;
 
 ALTER TABLE avatars_list DROP COLUMN `url`;
 
-ALTER TABLE `avatars_list` ADD COLUMN `url` VARCHAR(255) GENERATED ALWAYS AS (CONCAT('https://content.faforever.com/faf/avatars/', filename)) STORED;
+ALTER TABLE `avatars_list` ADD COLUMN `url` VARCHAR(255) GENERATED ALWAYS AS (CONCAT('https://content.faforever.com/faf/avatars/', filename)) VIRTUAL;
 
-ALTER TABLE `avatars_list` ADD CONSTRAINT `url` UNIQUE (`url`, `tooltip`);
+ALTER TABLE `avatars_list` ADD CONSTRAINT `filename` UNIQUE (`filename`, `tooltip`);
