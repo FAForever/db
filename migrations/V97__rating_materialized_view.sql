074830d8092968533a6bd6083a4125dae7dd17dc
Konstantin Kalinovsky <konstantin.kalinovsky@firma.seznam.cz>
2020-06-24 22:57:02 +0200

    #233 sql updated

--- migrations/V97__rating_materialized_view.sql
+++ migrations/V97__rating_materialized_view.sql
@@ -1,75 +1,65 @@
 CREATE TABLE IF NOT EXISTS ladder1v1_rating_rank_view
 (
-    id        mediumint unsigned             not null,
-    ranking   int,
-    mean      float                          null,
-    deviation float                          null,
-    numGames  smallint(4) unsigned default 0 not null,
-    winGames  smallint(4) unsigned default 0 not null,
-    is_active tinyint(1)           default 0 not null,
-    rating    float
-);
+    id MEDIUMINT UNSIGNED NOT NULL PRIMARY KEY COMMENT 'References to ladder1v1_rating.id',
+    ranking INTEGER UNIQUE AUTO_INCREMENT,
+    mean FLOAT NULL,
+    deviation FLOAT NULL,
+    num_games SMALLINT UNSIGNED DEFAULT 0,
+    win_games SMALLINT UNSIGNED DEFAULT 0,
+    rating FLOAT
+) COMMENT='Materialized view including ranks (player position) for ladder1v1_rating';
 
 CREATE TABLE IF NOT EXISTS global_rating_rank_view
 (
-    id        mediumint unsigned             not null,
-    ranking   int,
-    mean      float                          null,
-    deviation float                          null,
-    numGames  smallint(4) unsigned default 0 not null,
-    winGames  smallint(4) unsigned default 0 not null,
-    is_active tinyint(1)           default 0 not null,
-    rating    float
-);
+    id MEDIUMINT UNSIGNED NOT NULL PRIMARY KEY COMMENT 'References to global_rating.id',
+    ranking INTEGER UNIQUE AUTO_INCREMENT,
+    mean FLOAT NULL,
+    deviation FLOAT NULL,
+    num_games SMALLINT UNSIGNED DEFAULT 0,
+    rating FLOAT
+) COMMENT='Materialized view including ranks (player position) for global_rating';
 
 DELIMITER $$
 
 CREATE EVENT IF NOT EXISTS `ladder1v1_calculate_ranks_view`
-    ON SCHEDULE EVERY 5 MINUTE STARTS CURRENT_TIMESTAMP
-    COMMENT 'Fill the materialized view including ranks for ladder'
-    DO
-    BEGIN
-        START TRANSACTION;
-        SET @rankLadder = 0;
-        DELETE FROM ladder1v1_rating_rank_view;
-        INSERT INTO ladder1v1_rating_rank_view(id, ranking, mean, deviation, numGames, winGames, is_active, rating)
-        SELECT id,
-               @rankLadder := (@rankLadder + 1),
-               mean,
-               deviation,
-               numGames,
-               winGames,
-               is_active,
-               rating
-        FROM ladder1v1_rating
-        ORDER BY rating DESC;
-        COMMIT;
-    END $$
-
-DELIMITER ;
-
-DELIMITER $$
+ON SCHEDULE EVERY 1 HOUR STARTS CURRENT_TIMESTAMP
+COMMENT 'Fill the materialized view including ranks for ladder'
+DO
+BEGIN
+    ALTER TABLE ladder1v1_rating_rank_view AUTO_INCREMENT = 1, ALGORITHM=INPLACE;
+    START TRANSACTION;
+    DELETE FROM ladder1v1_rating_rank_view;
+    INSERT INTO ladder1v1_rating_rank_view(`id`, `mean`, `deviation`, `num_games`, `win_games`, `rating`)
+    SELECT id,
+           mean,
+           deviation,
+           numGames,
+           winGames,
+           rating
+    FROM ladder1v1_rating
+    WHERE is_active = 1
+    ORDER BY rating DESC;
+    COMMIT;
+END $$
 
 CREATE EVENT IF NOT EXISTS `global_calculate_ranks_view`
-    ON SCHEDULE EVERY 5 MINUTE STARTS CURRENT_TIMESTAMP
-    COMMENT 'Fill the materialized view including ranks for global'
-    DO
-    BEGIN
-        START TRANSACTION;
-        SET @rankGlobal = 0;
-        DELETE FROM global_rating_rank_view;
-        INSERT INTO global_rating_rank_view(id, ranking, mean, deviation, numGames, winGames, is_active, rating)
-        SELECT id,
-               @rankGlobal := (@rankGlobal + 1),
-               mean,
-               deviation,
-               numGames,
-               winGames,
-               is_active,
-               rating
-        FROM ladder1v1_rating
-        ORDER BY rating DESC;
-        COMMIT;
-    END $$
+ON SCHEDULE EVERY 1 HOUR STARTS CURRENT_TIMESTAMP
+COMMENT 'Fill the materialized view including ranks for global'
+DO
+BEGIN
+    ALTER TABLE global_rating_rank_view AUTO_INCREMENT = 1, ALGORITHM=INPLACE;
+    START TRANSACTION;
+    DELETE FROM global_rating_rank_view;
+    INSERT INTO global_rating_rank_view(`id`, `mean`, `deviation`, `num_games`, `rating`)
+    SELECT id,
+           mean,
+           deviation,
+           numGames,
+           rating
+    FROM global_rating
+    WHERE is_active = 1
+    ORDER BY rating DESC;
+    COMMIT;
+END $$
 
 DELIMITER ;
