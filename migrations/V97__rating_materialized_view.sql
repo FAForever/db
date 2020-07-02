bdf81b3154741866f7196d20640430cdc8400146
Brutus5000 <Brutus5000@gmx.net>
2020-07-02 21:00:52 +0200

    #233 Consider active and banned players

--- migrations/V97__rating_materialized_view.sql
+++ migrations/V97__rating_materialized_view.sql
@@ -26,19 +26,38 @@ ON SCHEDULE EVERY 1 HOUR STARTS CURRENT_TIMESTAMP
 COMMENT 'Fill the materialized view including ranks for ladder'
 DO
 BEGIN
+    CREATE TEMPORARY TABLE active_players ENGINE=MEMORY AS
+    (
+        SELECT DISTINCT gps.playerId
+        FROM game_player_stats gps
+                 INNER JOIN game_stats gs on gps.gameId = gs.id
+        WHERE gs.endTime > now() - INTERVAL 1 YEAR
+          AND gs.gameMod = 6 -- ladder
+          AND gs.validity = 0
+    );
+
+    DELETE FROM active_players
+    WHERE playerId IN (
+        SELECT player_id from ban
+        WHERE (expires_at is null or expires_at > NOW()) AND revoke_time IS NULL
+    );
+
     ALTER TABLE ladder1v1_rating_rank_view AUTO_INCREMENT = 1, ALGORITHM=INPLACE;
     START TRANSACTION;
-    DELETE FROM ladder1v1_rating_rank_view;
+    TRUNCATE ladder1v1_rating_rank_view;
     INSERT INTO ladder1v1_rating_rank_view(`id`, `mean`, `deviation`, `num_games`, `win_games`, `rating`)
-    SELECT id,
+    SELECT ladder1v1_rating.id,
            mean,
            deviation,
            numGames,
            winGames,
            rating
     FROM ladder1v1_rating
-    WHERE is_active = 1
+    INNER JOIN active_players ON ladder1v1_rating.id = active_players.playerId
     ORDER BY rating DESC;
+
+    DROP TABLE active_players;
+
     COMMIT;
 END $$
 
@@ -47,18 +66,39 @@ ON SCHEDULE EVERY 1 HOUR STARTS CURRENT_TIMESTAMP
 COMMENT 'Fill the materialized view including ranks for global'
 DO
 BEGIN
+    CREATE TEMPORARY TABLE active_players ENGINE=MEMORY AS
+    (
+        SELECT DISTINCT gps.playerId
+        FROM game_player_stats gps
+                 INNER JOIN game_stats gs on gps.gameId = gs.id
+        WHERE gs.endTime > now() - INTERVAL 1 YEAR
+          AND gs.gameMod = 0 -- global
+          AND gs.validity = 0
+    );
+
+    DELETE FROM active_players
+    WHERE playerId IN (
+        SELECT player_id from ban
+        WHERE (expires_at is null or expires_at > NOW()) AND revoke_time IS NULL
+    );
+
     ALTER TABLE global_rating_rank_view AUTO_INCREMENT = 1, ALGORITHM=INPLACE;
     START TRANSACTION;
-    DELETE FROM global_rating_rank_view;
+    TRUNCATE global_rating_rank_view;
     INSERT INTO global_rating_rank_view(`id`, `mean`, `deviation`, `num_games`, `rating`)
-    SELECT id,
+    SELECT global_rating.id,
            mean,
            deviation,
            numGames,
            rating
     FROM global_rating
-    WHERE is_active = 1
+    LEFT JOIN ban ON global_rating.id = ban.player_id
+    WHERE ((ban.expires_at is null or ban.expires_at > NOW()) AND ban.revoke_time IS NULL)
+      AND  is_active = 1
     ORDER BY rating DESC;
+
+    DROP TABLE active_players;
+
     COMMIT;
 END $$
 
