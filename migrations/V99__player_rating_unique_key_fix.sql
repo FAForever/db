015ed37a865d9b8cd5c9c54b7b3a593eb20ee537
Brutus5000 <Brutus5000@gmx.net>
2020-07-13 23:05:47 +0200

    Fix performance issue

--- migrations/V99__player_rating_unique_key_fix.sql
+++ migrations/V99__player_rating_unique_key_fix.sql
@@ -1,14 +1,23 @@
-DELETE FROM leaderboard_rating
-WHERE leaderboard_rating.id IN (
-    SELECT subq.id FROM (
-        SELECT r1.id
-        FROM leaderboard_rating AS r1
-        INNER JOIN leaderboard_rating AS r2 ON
-            r1.id != r2.id AND
-            r1.login_id = r2.login_id AND
-            r1.leaderboard_id = r2.leaderboard_id AND
-            r1.create_time > r2.create_time
-    ) as subq
-);
+-- Due to Travis issues, the statements below are only run on production.
+-- (They are not required on Travis anyway as the inconsistency only occurred in combination with a running lobby server)
+
+-- CREATE TEMPORARY TABLE duplicates
+-- (
+--     id INT,
+--     PRIMARY KEY (id)
+-- ) ENGINE = MEMORY AS
+-- SELECT r1.id
+-- FROM leaderboard_rating AS r1
+--          INNER JOIN leaderboard_rating AS r2 ON
+--         r1.id != r2.id AND
+--         r1.login_id = r2.login_id AND
+--         r1.leaderboard_id = r2.leaderboard_id AND
+--         r1.create_time > r2.create_time;
+--
+-- DELETE lr
+-- FROM leaderboard_rating lr
+-- INNER JOIN duplicates d ON lr.id = d.id;
+--
+-- DROP TEMPORARY TABLE duplicates;
 
 ALTER TABLE leaderboard_rating ADD UNIQUE (login_id, leaderboard_id);
