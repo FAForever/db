3a0f75271ab3c84d3b1809263b86d9e8d7a762e8
Clemens Borys <clemens@borys.dk>
2020-09-07 14:26:30 +0200

    Add matchmaker_queue_game table

---
+++ migrations/V103__link_matchmaker_games_to_originating_queue.sql
@@ -0,0 +1,9 @@
+CREATE TABLE matchmaker_queue_game
+(
+  id                  INT(10) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  matchmaker_queue_id INT(10) UNSIGNED NOT NULL,
+  game_stats_id       INT(10) UNSIGNED NOT NULL,
+  FOREIGN KEY (matchmaker_queue_id) REFERENCES matchmaker_queue (id),
+  FOREIGN KEY (game_stats_id) REFERENCES game_stats (id),
+  UNIQUE INDEX (game_stats_id, matchmaker_queue_id)
+);
