67120a3ea623a709fa664c254d06a67142279be2
Clemens Borys <clemens@borys.dk>
2020-09-14 15:42:20 +0200

    Old game cleanup (#231)
    * Add gameMod=6 to old ladder games and make games with repeated players invalid
    
    * Use BETWEEN, drop collation
    
    * Save player count in temporary table
    
    * Remove database prefixes
    
    * Filter temporary table with HAVING
    
    * Collate column instead of literal
    
    * Rename V95 -> V97
    
    * Format and move to version 103

---
+++ migrations/V103__laddermod_cleanup.sql
@@ -0,0 +1,58 @@
+UPDATE
+  game_stats
+SET
+  gameMod = 6
+WHERE
+  gameMod = 0
+AND
+  startTime < "2014-03-22 12:59:25"
+AND
+  gameName COLLATE utf8mb4_bin LIKE "% Vs %"
+AND
+  (
+    SELECT count(*) 
+    FROM game_player_stats
+    WHERE gameId = game_stats.id
+  ) = 2
+AND
+  (
+    SELECT count(*) 
+    FROM game_player_stats
+    WHERE gameId = game_stats.id AND
+      AI = false AND
+      color IN (1, 2) AND
+      score in (-1, 0, 1)
+  ) = 2
+;
+
+
+DROP TEMPORARY TABLE IF EXISTS game_player_count;
+
+CREATE TEMPORARY TABLE game_player_count
+    SELECT
+      game_stats.id AS id,
+      count(game_player_stats.id) AS gps_row_count,
+      count(DISTINCT game_player_stats.playerId) AS distinct_player_count
+    FROM 
+      game_stats
+    INNER JOIN 
+      game_player_stats ON (game_player_stats.gameId = game_stats.id)
+    WHERE
+      game_stats.id BETWEEN 4393127 AND 4424727
+    GROUP BY
+      game_stats.id
+    HAVING
+      gps_row_count != distinct_player_count
+;
+
+UPDATE
+  game_stats
+INNER JOIN
+  game_player_count ON (game_stats.id = game_player_count.id)
+SET
+  game_stats.validity = 24  # "Unranked for another reason"
+WHERE
+  game_stats.validity = 0
+;
+
+DROP TEMPORARY TABLE game_player_count;
