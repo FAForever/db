cac2c586cd039d2c26a271ec813fe426618cbb1e
Brutus5000 <Brutus5000@gmx.net>
2021-02-13 01:04:43 +0100

    Migrate to Github actions

--- ci/test-data.sh
+++ ci/test-data.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 set -e
 
-echo 'travis_fold:start:test_data'
 echo '# Import test data'
 
 echo "Insert test-data.sql for 1st run"
-docker exec -i faf-db mysql -uroot -pbanana faf < test-data.sql || { echo "Test data failed"; exit 1; };
+mysql -h "${MYSQL_HOST}" -u root -p"${MYSQL_ROOT_PASSWORD}" "${MYSQL_DATABASE}" < test-data.sql || { echo "Test data failed"; exit 1; };
 
 echo "Insert test-data.sql for 2nd run (ensure cleanup works as well)"
-docker exec -i faf-db mysql -uroot -pbanana faf < test-data.sql || { echo "Test data failed"; exit 1; };
-
-echo 'travis_fold:end:test_data'
+mysql -h "${MYSQL_HOST}" -u root -p"${MYSQL_ROOT_PASSWORD}" "${MYSQL_DATABASE}" < test-data.sql || { echo "Test data failed"; exit 1; };
