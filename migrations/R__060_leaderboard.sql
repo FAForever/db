4adb45ffee05bae85c4e311db0b0352a58819336
Askaholic <askaholic907@gmail.com>
2021-03-13 14:40:00 -0900

    Add initialization column to leaderboard

--- migrations/R__060_leaderboard.sql
+++ migrations/R__060_leaderboard.sql
@@ -4,12 +4,13 @@
 -- If you want to edit an existing or add a new leaderboard, just append it to the SQL command and run flyway migrate.
 -- Leaderboards are handled dynamically. You can add leaderboards and reference them from the matchmaker.
 
-INSERT INTO `leaderboard` (id, technical_name, name_key, description_key) VALUES
-(1,'global','leaderboard.global.name','leaderboard.global.description'),
-(2,'ladder_1v1','leaderboard.ladder_1v1.name','leaderboard.ladder_1v1.description'),
-(3,'tmm_2v2','leaderboard.tmm_2v2.name','leaderboard.tmm_2v2.description')
+INSERT INTO `leaderboard` (id, initializer_id, technical_name, name_key, description_key) VALUES
+(1,NULL,'global','leaderboard.global.name','leaderboard.global.description'),
+(2,NULL,'ladder_1v1','leaderboard.ladder_1v1.name','leaderboard.ladder_1v1.description'),
+(3,1,'tmm_2v2','leaderboard.tmm_2v2.name','leaderboard.tmm_2v2.description')
 -- add new row above this comment (don't forget to append the comma to the previous one)
 ON DUPLICATE KEY UPDATE
+    initializer_id=VALUES(initializer_id),
     technical_name=VALUES(technical_name),
     name_key=VALUES(name_key),
     description_key=VALUES(description_key);
