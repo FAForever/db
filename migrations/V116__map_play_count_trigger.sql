f93333614266aef8314a36d6ba472b3097452bc6
Sheikah45 <Sheikah450@gmail.com>
2021-04-24 16:37:46 -0400

    Rename times_played to games_played and define triggers before update

--- migrations/V116__map_play_count_trigger.sql
+++ migrations/V116__map_play_count_trigger.sql
@@ -1,10 +1,10 @@
--- Add times_played to map_version
+-- Add games_played to map_version
 ALTER TABLE map_version
-    ADD COLUMN times_played int unsigned NOT NULL DEFAULT 0 COMMENT 'Number of times a map version has been played. Should be updated each time a row is inserted with the map_version.id in game_stats. Values start at 0' AFTER reviews;
+    ADD COLUMN games_played int unsigned NOT NULL DEFAULT 0 COMMENT 'Number of times a map version has been played. Should be updated each time a row is inserted with the map_version.id in game_stats. Values start at 0' AFTER reviews;
 
--- Add times_played to map_version
+-- Add games_played to map_version
 ALTER TABLE map
-    ADD COLUMN times_played int unsigned NOT NULL DEFAULT 0 COMMENT 'Number of times a map version has been played. Should be updated each time times_played is updated in map_version Values start at 0' AFTER reviews;
+    ADD COLUMN games_played int unsigned NOT NULL DEFAULT 0 COMMENT 'Number of times a map version has been played. Should be updated each time games_played is updated in map_version Values start at 0' AFTER reviews;
 
 DROP TABLE IF EXISTS table_map_features;
 
@@ -12,41 +12,13 @@ DROP TABLE IF EXISTS table_map_features;
 DROP VIEW IF EXISTS map_statistics;
 
 CREATE DEFINER=root@localhost VIEW map_statistics AS
-    SELECT id, 0 AS downloads, times_played, 0 AS num_draws FROM map;
+SELECT id, 0 AS downloads, games_played AS times_played, 0 AS num_draws FROM map;
 
 -- Update view for backwards compatibility with api
 DROP VIEW IF EXISTS map_version_statistics;
 
 CREATE DEFINER=root@localhost VIEW map_version_statistics AS
-    SELECT id, 0 AS downloads, times_played, 0 AS num_draws FROM map_version;
-
--- one time update of map_version to get times_played to the right play_count
-DROP TABLE IF EXISTS map_version_play_count;
-
-CREATE TEMPORARY TABLE map_version_play_count AS
-    SELECT mapId, COUNT(DISTINCT id) AS times_played
-    FROM game_stats
-    GROUP BY mapId;
-
-UPDATE map_version, map_version_play_count
-SET map_version.times_played = map_version_play_count.times_played
-WHERE map_version.id = map_version_play_count.mapId;
-
-DROP TABLE IF EXISTS map_version_play_count;
-
--- one time update of map to get times_played to the right play_count
-DROP TABLE IF EXISTS map_play_count;
-
-CREATE TEMPORARY TABLE map_play_count AS
-SELECT map_version.map_id, SUM(map_version.times_played) AS times_played
-FROM map_version
-GROUP BY map_version.map_id;
-
-UPDATE map, map_play_count
-SET map.times_played = map_play_count.times_played
-WHERE map.id = map_play_count.map_id;
-
-DROP TABLE IF EXISTS map_play_count;
+SELECT id, 0 AS downloads, games_played AS times_played, 0 AS num_draws FROM map_version;
 
 -- Add trigger to increment map_version play count
 DELIMITER ;
@@ -56,9 +28,9 @@ DELIMITER $$
 $$
 CREATE DEFINER=root@localhost TRIGGER map_version_play_count AFTER INSERT ON game_stats FOR EACH ROW
 BEGIN
-    -- "Increment times_played in map_version on new insertion to game_stats';
+    -- "Increment games_played in map_version on new insertion to game_stats';
     UPDATE map_version
-    SET times_played = (times_played + 1)
+    SET games_played = (games_played + 1)
     WHERE map_version.id = NEW.mapId;
 END;$$
 
@@ -70,11 +42,25 @@ DELIMITER $$
 $$
 CREATE DEFINER=root@localhost TRIGGER map_play_count AFTER UPDATE ON map_version FOR EACH ROW
 BEGIN
-    -- "Increment times_played in map on updated times_played in map_version';
-    IF NEW.times_played != OLD.times_played
+    -- "Increment games_played in map on updated games_played in map_version';
+    IF NEW.games_played != OLD.games_played
     THEN
         UPDATE map
-        SET times_played = (times_played + (NEW.times_played - OLD.times_played))
+        SET games_played = (games_played + (NEW.games_played - OLD.games_played))
         WHERE map.id = NEW.map_id;
     END IF;
 END;$$
+
+-- one time update of map_version to get games_played to the right play_count
+DROP TABLE IF EXISTS map_version_play_count;
+
+CREATE TEMPORARY TABLE map_version_play_count AS
+    SELECT mapId, COUNT(DISTINCT id) AS games_played
+    FROM game_stats
+    GROUP BY mapId;
+
+UPDATE map_version, map_version_play_count
+SET map_version.games_played = map_version_play_count.games_played
+WHERE map_version.id = map_version_play_count.mapId;
+
+DROP TABLE IF EXISTS map_version_play_count;
