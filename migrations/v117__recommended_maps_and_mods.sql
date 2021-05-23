570ee2cff4ef6b760935616238dc62944d1e5a3c
Sheikah45 <Sheikah450@gmail.com>
2021-05-23 18:27:26 -0400

    Add recommended column to map and mod tables

---
+++ migrations/v117__recommended_maps_and_mods.sql
@@ -0,0 +1,7 @@
+-- Add recommended column to map
+ALTER TABLE `map`
+    ADD COLUMN `recommended` boolean NOT NULL DEFAULT false COMMENT 'Boolean to indicate if a map is a high quality FAF Map' AFTER `games_played`;
+
+-- Add recommended column to mod
+ALTER TABLE `mod`
+    ADD COLUMN `recommended` boolean NOT NULL DEFAULT false COMMENT 'Boolean to indicate if a map is a high quality FAF Mod' AFTER `reviews`;
