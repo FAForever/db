f8ad97f4e113ed67d0081d7bb1f5b8e8ff6e9ba7
BlackYps <jonas-fb@gmx.de>
2021-11-04 15:28:21 +0100

    Add new leaderboards and matchmaker queues

--- migrations/R__070_matchmaker_queue.sql
+++ migrations/R__070_matchmaker_queue.sql
@@ -4,9 +4,11 @@
 -- If you want to edit an existing or add a new matchmaker queue, just append it to the SQL command and run flyway migrate.
 -- Matchmaker queues are handled dynamically. If they are enabled they will show up. (The map pool is managed by privileged users.)
 
-INSERT INTO `matchmaker_queue` (id, technical_name, featured_mod_id, leaderboard_id, name_key, team_size, enabled) VALUES
-(1,'ladder1v1',6,2,'matchmaker_queue.ladder1v1',1,1),
-(2,'tmm2v2',0,3,'matchmaker_queue.tmm2v2.name',2,1)
+INSERT INTO `matchmaker_queue` (id, technical_name, featured_mod_id, leaderboard_id, name_key, team_size, params, enabled) VALUES
+(1,'ladder1v1',6,2,'matchmaker_queue.ladder1v1',1,'UnitCap: 1500',1),
+(2,'tmm2v2',0,3,'matchmaker_queue.tmm2v2.name',2,NULL,1),
+(3,'tmm4v4FullShare',0,4,'matchmaker_queue.tmm4v4FullShare.name',4,NULL,1),
+(4,'tmm4v4ShareUntilDeath',0,5,'matchmaker_queue.tmm4v4ShareUntilDeath.name',4,'Share: ShareUntilDeath',1)
 -- add new row above this comment (don't forget to append the comma to the previous one)
 ON DUPLICATE KEY UPDATE
     technical_name=VALUES(technical_name),
