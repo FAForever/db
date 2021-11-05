0a820605ec9f49ebac4827ddae1c34bd03a4d06e
BlackYps <jonas-fb@gmx.de>
2021-11-05 21:57:43 +0100

    change ladder1v1 featured mod to faf

--- migrations/R__070_matchmaker_queue.sql
+++ migrations/R__070_matchmaker_queue.sql
@@ -5,7 +5,7 @@
 -- Matchmaker queues are handled dynamically. If they are enabled they will show up. (The map pool is managed by privileged users.)
 
 INSERT INTO `matchmaker_queue` (id, technical_name, featured_mod_id, leaderboard_id, name_key, team_size, params, enabled) VALUES
-(1,'ladder1v1',6,2,'matchmaker_queue.ladder1v1',1,'UnitCap: 1500',1),
+(1,'ladder1v1',0,2,'matchmaker_queue.ladder1v1',1,'UnitCap: 1500',1),
 (2,'tmm2v2',0,3,'matchmaker_queue.tmm2v2.name',2,NULL,1),
 (3,'tmm4v4_full_share',0,4,'matchmaker_queue.tmm4v4_full_share.name',4,NULL,0),
 (4,'tmm4v4_share_until_death',0,5,'matchmaker_queue.tmm4v4_share_until_death.name',4,'Share: ShareUntilDeath',0)
