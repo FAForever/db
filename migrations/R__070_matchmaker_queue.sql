25edb88a8a67ab9d81756fa7762040432905c429
BlackYps <jonas-fb@gmx.de>
2021-11-05 21:53:27 +0100

    Change queue names to snake case

--- migrations/R__070_matchmaker_queue.sql
+++ migrations/R__070_matchmaker_queue.sql
@@ -7,8 +7,8 @@
 INSERT INTO `matchmaker_queue` (id, technical_name, featured_mod_id, leaderboard_id, name_key, team_size, params, enabled) VALUES
 (1,'ladder1v1',6,2,'matchmaker_queue.ladder1v1',1,'UnitCap: 1500',1),
 (2,'tmm2v2',0,3,'matchmaker_queue.tmm2v2.name',2,NULL,1),
-(3,'tmm4v4FullShare',0,4,'matchmaker_queue.tmm4v4FullShare.name',4,NULL,0),
-(4,'tmm4v4ShareUntilDeath',0,5,'matchmaker_queue.tmm4v4ShareUntilDeath.name',4,'Share: ShareUntilDeath',0)
+(3,'tmm4v4_full_share',0,4,'matchmaker_queue.tmm4v4_full_share.name',4,NULL,0),
+(4,'tmm4v4_share_until_death',0,5,'matchmaker_queue.tmm4v4_share_until_death.name',4,'Share: ShareUntilDeath',0)
 -- add new row above this comment (don't forget to append the comma to the previous one)
 ON DUPLICATE KEY UPDATE
     technical_name=VALUES(technical_name),
