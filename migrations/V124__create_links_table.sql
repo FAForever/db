0cfeda68dad6680486959f2a9cf4435d538ff05d
Sheikah <Sheikah450@gmail.com>
2022-01-03 17:26:34 -0500

    Remove hash

--- migrations/V124__create_links_table.sql
+++ migrations/V124__create_links_table.sql
@@ -5,8 +5,7 @@ CREATE TABLE `service_links` (
                          `user_id` mediumint(8) unsigned COMMENT "To be set to null if account is deleted",
                          `type` enum('STEAM', 'GOG', 'DISCORD', 'PATREON') COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'The service that this service id links to.',
                          `service_id` varchar(100) COMMENT "To be set to null if account is deleted",
-                         `hashed_id` char(64) NOT NULL,
-                         `hash_salt` char(100) NOT NULL,
+                         `service_hash_id` char(64) NOT NULL
                          `public` boolean,
                          `ownership` boolean,
                          PRIMARY KEY (uuid),
@@ -16,14 +15,13 @@ CREATE TABLE `service_links` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='table representing the links to various services';
 
 # MIGRATION SCRIPT
-# SET @SECRET = "{SECRET}";
 #
-# INSERT into `service_links` (`user_id`, `type`, `service_id`, `hashed_id`, `hash_salt`, `public`)
-# SELECT `id`, "STEAM", `steamid`, SHA2(CONCAT(`steamid`, @SECRET), 256) as `hashed_id`, @SECRET, false
+# INSERT into `service_links` (`user_id`, `type`, `service_id`, `ownership`, `public`)
+# SELECT `id`, "STEAM", `steamid`, true, false
 # FROM `login` WHERE `steamid` IS NOT NULL
 # ON DUPLICATE KEY UPDATE `user_id`=`user_id`;
 #
-# INSERT into `service_links` (`user_id`, `type`, `service_id`, `hashed_id`, `hash_salt`, `public`)
-# SELECT `id`, "GOG", `gog_id`, SHA2(CONCAT(`gog_id`, @SECRET), 256) as `hashed_id`, @SECRET, false
+# INSERT into `service_links` (`user_id`, `type`, `service_id`, `ownership`, `public`)
+# SELECT `id`, "GOG", `gog_id`, false, false
 # FROM `login` WHERE `gog_id` IS NOT NULL
 # ON DUPLICATE KEY UPDATE `user_id`=`user_id`;
