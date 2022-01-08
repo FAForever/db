e76fadc6cb020de4b3a07ebc922b83db1f78580a
Sheikah <Sheikah450@gmail.com>
2022-01-08 09:01:22 -0500

    Add comments

--- migrations/V124__create_links_table.sql
+++ migrations/V124__create_links_table.sql
@@ -1,15 +1,14 @@
 DROP TABLE IF EXISTS `service_links`;
 
 CREATE TABLE `service_links` (
-                         `uuid` uuid unsigned NOT NULL,
-                         `user_id` mediumint(8) unsigned COMMENT "To be set to null if account is deleted",
+                         `id` CHAR(36) CHARACTER SET ascii NOT NULL,
+                         `user_id` mediumint(8) unsigned COMMENT 'To be set to null if account is deleted',
                          `type` enum('STEAM', 'GOG', 'DISCORD', 'PATREON') COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'The service that this service id links to.',
-                         `service_id` varchar(100) COMMENT "To be set to null if account is deleted",
+                         `service_id` varchar(100) COMMENT 'To be set to null if account is deleted',
                          `public` boolean,
-                         `ownership` boolean,
-                         PRIMARY KEY (uuid),
+                         `ownership` boolean COMMENT 'If true, this link confirms that the user owns FAForever. If true the link must never be deleted even if the linked account gets deleted. In this case we keep a dangling link so the id cannot be reused',
+                         PRIMARY KEY (id),
                          UNIQUE KEY `unique_service_id` (`type`, `service_id`),
-                         UNIQUE KEY `unique_hashed_id` (`type`, `hashed_id`),
                          CONSTRAINT `user_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `login` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='table representing the links to various services';
 
@@ -21,6 +20,6 @@ CREATE TABLE `service_links` (
 # ON DUPLICATE KEY UPDATE `user_id`=`user_id`;
 #
 # INSERT into `service_links` (`user_id`, `type`, `service_id`, `ownership`, `public`)
-# SELECT `id`, "GOG", `gog_id`, false, false
+# SELECT `id`, "GOG", `gog_id`, true, false
 # FROM `login` WHERE `gog_id` IS NOT NULL
 # ON DUPLICATE KEY UPDATE `user_id`=`user_id`;
