882820614e82691d2e0b3e2b82aba1d9718c7fdf
Sheikah45 <66929319+Sheikah45@users.noreply.github.com>
2022-07-17 18:07:14 -0400

    filter out null id rows

--- migrations/V126__create_links_table.sql
+++ migrations/V126__create_links_table.sql
@@ -16,7 +16,8 @@ CREATE TABLE `service_links` (
 # drop table if exists temp_id_holder;
 #
 # create temporary table temp_id_holder as
-# select * from login order by RAND();
+# select * from login order by RAND()
+# WHERE `steamid` IS NOT NULL or `gog_id` IS NOT NULL;
 #
 # INSERT into `service_links` (`id`, `user_id`, `type`, `service_id`, `ownership`, `public`)
 # SELECT UUID(), `id`, "STEAM", `steamid`, true, false
