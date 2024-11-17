920498ff5c4e9134979c195dd0b1e0137b43c096
Kirill Riman <55841348+K-ETFreeman@users.noreply.github.com>
2024-11-17 12:29:35 +0300

    column rename + file ext fix

--- migrations/V135__add_veto_fields_to_matchmaker_queue_map_pool.sql
+++ migrations/V135__add_veto_fields_to_matchmaker_queue_map_pool.sql
@@ -1,4 +1,4 @@
 ALTER TABLE matchmaker_queue_map_pool
   ADD COLUMN veto_tokens_per_player TINYINT UNSIGNED DEFAULT 0 NOT NULL,
   ADD COLUMN max_tokens_per_map TINYINT UNSIGNED DEFAULT 0 NOT NULL,
-  ADD COLUMN minimal_maps_allowed FLOAT DEFAULT 1 NOT NULL CHECK (minimal_maps_allowed >= 1);
+  ADD COLUMN minimum_maps_after_veto FLOAT DEFAULT 1 NOT NULL CHECK (minimum_maps_after_veto > 0);
